3afedafdb4ed97ebfd63a9484b609aa51cb53cd2
salemashraf <salemashraf2002@gmail.com>
2025-06-23 02:58:48 +0300

    blade files

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/index', function () {
+    return view('index');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/post', function () {
+    return view('post');
+});
+
